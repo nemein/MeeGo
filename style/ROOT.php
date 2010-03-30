3f708b6f309377c49ebc8bd9691d9db50fb403ec
Ferenc Szekely <ferenc.szekely@gmail.com>
2010-03-30 23:10:25 +0300

    enabling jQuery for all users

--- style/ROOT.php
+++ style/ROOT.php
@@ -1,5 +1,6 @@
 <?php
   $topic = $_MIDCOM->get_context_data(MIDCOM_CONTEXT_CONTENTTOPIC);
+  $_MIDCOM->enable_jquery();
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
