c2f19ad84328b85667744639c38bc35f2a33deac
Ferenc Szekely <ferenc.szekely@gmail.com>
2010-11-19 11:41:22 +0200

    component config snippets

---
+++ snippets/sitegroup-config/org.maemo.socialnews/config.php
@@ -0,0 +1,8 @@
+'frontpage_score_start' => 0.5,
+'favourites_modifier' => 1,
+'show_navigation_pseudo_leaves' => false,
+'frontpage_limit_days' => 100,
+'maemo_favourites_url' => '/favourites',
+'show_navigation_pseudo_leaves' => false,
+'delicious_enable' => true,
+'delicious_modifier' => 1,
