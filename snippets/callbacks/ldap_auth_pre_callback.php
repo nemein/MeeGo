f2ea1d10eb9e7f48a48af712b285547d7997ff4b
Ferenc Szekely <ferenc.szekely@gmail.com>
2010-11-23 14:50:19 +0200

    fixes in _ldap_search

--- snippets/callbacks/ldap_auth_pre_callback.php
+++ snippets/callbacks/ldap_auth_pre_callback.php
@@ -9,8 +9,8 @@ ldap_auth_pre_callback($argv[1]);
  * if user exists in LDAP then create an account in db
  * if user does not exist then .. give up :)
  */
-function ldap_auth_pre_callback($username)
-    var_dump(_ldap_search($criteria));
+function ldap_auth_pre_callback($username) {
+    var_dump(_ldap_search($username));
     return;
 }
 
@@ -25,45 +25,36 @@ function _ldap_search($criteria)
     $retval = null;
     if (isset($criteria))
     {
-        $criteria .= 'uid=' . $criteria
+        $criteria .= 'uid=' . $criteria;
 
         $ds = ldap_connect("ldaps://ldap1.meego.com");
         echo "Connect result is " . $ds . "\n";
 
         if ($ds)
         {
-        echo "Searching for $criteria\n";
+            echo "Searching for $criteria\n";
 
-        // Search surname entry
-        $sr = ldap_search($ds, "ou=People,dc=meego,dc=com", $criteria);
+            // Search surname entry
+            $sr = ldap_search($ds, "ou=People,dc=meego,dc=com", $criteria);
 
-        echo "Search result is: " . $sr . "\n";
-        echo "Number of entires returned is: " . ldap_count_entries($ds, $sr) . "\n";
-        echo "Getting entries ...\n";
+            echo "Search result is: " . $sr . "\n";
+            echo "Number of entires returned is: " . ldap_count_entries($ds, $sr) . "\n";
+            echo "Getting entries ...\n";
 
-        $info = ldap_get_entries($ds, $sr);
+            $info = ldap_get_entries($ds, $sr);
 
-        echo "Data for " . $info["count"] . " items returned:\n";
+            echo "Data for " . $info["count"] . " items returned:\n";
 
-        if ($info['count'] > 0) {
-            $retval = array(
-              'username' => $info[0]["uid"][0];
-              'firtname' => $info[0]["cn"][0];
-              'email' => $info[0]["mail"][0];
-            );
+            if ($info['count'] > 0) {
+                $retval = array(
+                    'username' => $info[0]["uid"][0],
+                    'firtname' => $info[0]["cn"][0],
+                    'email' => $info[0]["mail"][0]
+                );
+            }
+            echo "Closing connection\n";
+            ldap_close($ds);
         }
-
-/*
-        for ($i = 0; $i < $info["count"]; $i++)
-        {
-            echo "employeenumber: " . $info[$i]["employeenumber"][0] . "\n";
-            echo "first cn entry is: " . $info[$i]["cn"][0] . "\n";
-            echo "first email entry is: " . $info[$i]["mail"][0] . "\n";
-            echo "---\n";
-        }
-*/
-        echo "Closing connection\n";
-        ldap_close($ds);
     }
     else
     {
